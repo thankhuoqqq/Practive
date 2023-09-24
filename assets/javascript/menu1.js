451287e494171fc74b86b10c9fc210c0042e105e
Thanh Huong <nguyenthithanhhuong181203@gmail.com>
2023-09-24 18:41:28 +0700

    .

---
+++ assets/javascript/menu1.js
@@ -0,0 +1,12 @@
+// Modal sunflower
+var modalProduct1 = document.getElementById('product1');
+        
+var btnProduct1 = document.getElementById('i__product1');
+
+btnProduct1.onclick = function() {
+    product1.style.display = "flex"
+}
+
+modalProduct1.onclick = function() {
+    product1.style.display = "none"
+}
